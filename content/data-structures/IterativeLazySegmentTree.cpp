d5f376f943051a2de491b43d3fb055f5aa921209
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-02-26 10:59:55 -0300

    Fix style in `IterativeLazySegmentTree`

--- content/data-structures/IterativeLazySegmentTree.cpp
+++ content/data-structures/IterativeLazySegmentTree.cpp
@@ -1,6 +1,6 @@
 const int inf = 1e9;
 
-struct LTree {
+struct Tree {
 	typedef int T; // data type
 	struct L { int mset, madd; }; // lazy type
 	const T tneut = -inf;		 // neutral elements
@@ -18,13 +18,10 @@ struct LTree {
 	vector<T> s;
 	vector<L> l;
 
-	LTree(int n = 0) : n(n), s(2*n, tneut), l(2*n, lneut) {}
-	LTree(const vector<T>& vals) : n(vals.size()), s(2*n), l(2*n, lneut) {
+	Tree(int n = 0) : n(n), s(2*n, tneut), l(2*n, lneut) {}
+	Tree(const vector<T>& vals) : n(vals.size()), s(2*n), l(2*n, lneut) {
 		copy(all(vals), begin(s) + n);
 		for (int i = n - 1; i > 0; i--) upd(i);
-	}
-	void upd(int p) {
-		if (p < n) s[p] = f(s[p*2], s[p*2+1]);
 	}
 	void push(int p, L a) {
 		l[p] = comb(l[p], a);
@@ -38,21 +35,24 @@ struct LTree {
 	void pull(int p) {
 		for (int h = __lg(p) + 1; h >= 0; h--) push(p >> h, lneut);
 	}
+	void upd(int p) {
+		if (p < n) s[p] = f(s[p*2], s[p*2+1]);
+	}
 	void update(int b, int e, L a) { // update [b, e)
 		pull(b += n);
-    pull((e += n) - 1);
-    for (; b < e; b /= 2, e /= 2) {
+		pull((e += n) - 1);
+		for (; b < e; b /= 2, e /= 2) {
 			if (b % 2) push(b++, a);
 			else upd(b);
 			if (e % 2) push(--e, a);
 			else upd(e);
 		}
-    for (; b > 0; b /= 2) upd(b);
+		for (; b > 0; b /= 2) upd(b);
 	}
 	T query(int b, int e) { // query [b, e)
 		T ra = tneut, rb = tneut;
 		pull(b += n);
-    pull((e += n) - 1);
+		pull((e += n) - 1);
 		for (; b < e; b /= 2, e /= 2) {
 			if (b % 2) ra = f(ra, s[b++]);
 			if (e % 2) rb = f(s[--e], rb);
