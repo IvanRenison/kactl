fb7c8e93aac863f483a380c6a47b83492f5f7f2a
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-03-01 16:33:24 -0300

    Use `ll` instead of `int`

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -15,26 +15,26 @@
 #pragma once
 
 struct STree { // example: range sum with range addition
-  typedef int T; typedef int L; // T: data type, L: lazy type
+  typedef ll T; typedef ll L; // T: data type, L: lazy type
   constexpr static T tneut = 0; constexpr static L lneut = 0;
   // neutrals
   T oper(T a, T b) { return a + b; }
-  T apply(T v, L l, int s, int e) { return v + l * (e - s); }
+  T apply(T v, L l, ll s, ll e) { return v + l * (e - s); }
   // new st according to lazy
   L prop(L a, L b) { return a + b; } // cumulative effect of lazy
 
   vector<T> st;
   vector<L> lazy;
-  int n;
-  STree(int n) : st(4*n+5, tneut), lazy(4*n+5, lneut), n(n) {}
-  void init(int k, int s, int e, const vector<T> &a) {
+  ll n;
+  STree(ll n) : st(4*n+5, tneut), lazy(4*n+5, lneut), n(n) {}
+  void init(ll k, ll s, ll e, const vector<T> &a) {
     lazy[k] = lneut;
     if (s + 1 == e) { st[k] = a[s]; return; }
-    int m = (s + e) / 2;
+    ll m = (s + e) / 2;
     init(2*k, s, m, a), init(2*k+1, m, e, a);
     st[k] = oper(st[2*k], st[2*k+1]);
   }
-  void push(int k, int s, int e) {
+  void push(ll k, ll s, ll e) {
     if (lazy[k] == lneut) return; // if neutral, nothing to do
     st[k] = apply(st[k], lazy[k], s, e);
     if (s + 1 < e) { // propagate to children
@@ -43,7 +43,7 @@ struct STree { // example: range sum with range addition
     }
     lazy[k] = lneut; // clear node lazy
   }
-  void upd(int k, int s, int e, int a, int b, L v) {
+  void upd(ll k, ll s, ll e, ll a, ll b, L v) {
     push(k, s, e);
     if (s >= b || e <= a) return;
     if (s >= a && e <= b) {
@@ -51,19 +51,19 @@ struct STree { // example: range sum with range addition
       push(k, s, e);
       return;
     }
-    int m = (s + e) / 2;
+    ll m = (s + e) / 2;
     upd(2*k, s, m, a, b, v), upd(2*k+1, m, e, a, b, v);
     st[k] = oper(st[2*k], st[2*k+1]);
   }
-  T query(int k, int s, int e, int a, int b) {
+  T query(ll k, ll s, ll e, ll a, ll b) {
     if (s >= b || e <= a) return tneut;
     push(k, s, e);
     if (s >= a && e <= b) return st[k];
-    int m = (s + e) / 2;
+    ll m = (s + e) / 2;
     return
       oper(query(2*k, s, m, a, b), query(2*k+1, m, e, a, b));
   }
   void init(const vector<T> &a) { init(1, 0, n, a); }
-  void upd(int a, int b, L v) { upd(1, 0, n, a, b, v); }
-  T query(int a, int b) { return query(1, 0, n, a, b); }
+  void upd(ll a, ll b, L v) { upd(1, 0, n, a, b, v); }
+  T query(ll a, ll b) { return query(1, 0, n, a, b); }
 };
--- stress-tests/data-structures/MemoryLazySegmentTree.cpp
+++ stress-tests/data-structures/MemoryLazySegmentTree.cpp
@@ -3,43 +3,43 @@
 #include "../../content/data-structures/MemoryLazySegmentTree.h"
 
 static unsigned R;
-int ra() {
+ll ra() {
 	R *= 791231;
 	R += 1231;
-	return (int)(R >> 1);
+	return (ll)(R >> 1);
 }
 
-volatile int res;
-int main() {
-	int N = 10;
-	vi v(N);
+volatile ll res;
+ll main() {
+	ll N = 10;
+	vector<ll> v(N);
 	iota(all(v), 0);
-	random_shuffle(all(v), [](int x) { return ra() % x; });
+	random_shuffle(all(v), [](ll x) { return ra() % x; });
 	Node* tr = new Node(v,0,N);
-	rep(i,0,N) rep(j,0,N) if (i <= j) {
-		int ma = -inf;
-		rep(k,i,j) ma = max(ma, v[k]);
+	fore(i,0,N) fore(j,0,N) if (i <= j) {
+		ll ma = -inf;
+		fore(k,i,j) ma = max(ma, v[k]);
 		assert(ma == tr->query(i,j));
 	}
-	rep(it,0,1000000) {
-		int i = ra() % (N+1), j = ra() % (N+1);
+	fore(it,0,1000000) {
+		ll i = ra() % (N+1), j = ra() % (N+1);
 		if (i > j) swap(i, j);
-		int x = (ra() % 10) - 5;
+		ll x = (ra() % 10) - 5;
 
-		int r = ra() % 100;
+		ll r = ra() % 100;
 		if (r < 30) {
 			::res = tr->query(i, j);
-			int ma = -inf;
-			rep(k,i,j) ma = max(ma, v[k]);
+			ll ma = -inf;
+			fore(k,i,j) ma = max(ma, v[k]);
 			assert(ma == ::res);
 		}
 		else if (r < 70) {
 			tr->add(i, j, x);
-			rep(k,i,j) v[k] += x;
+			fore(k,i,j) v[k] += x;
 		}
 		else {
 			tr->set(i, j, x);
-			rep(k,i,j) v[k] = x;
+			fore(k,i,j) v[k] = x;
 		}
 	}
 	cout<<"Tests passed!"<<endl;
