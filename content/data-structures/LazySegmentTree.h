5301518d10118bdec431a321bc126204a1674e97
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-03-02 20:00:32 -0300

    Rename `oper`  → `f` and `prop` to `comb` (to have the same names that in `MemoryLazySegmentTree` and delete unnecessary `+5`

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -18,28 +18,29 @@ struct STree { // example: range sum with range addition
   typedef ll T; typedef ll L; // T: data type, L: lazy type
   constexpr static T tneut = 0; constexpr static L lneut = 0;
   // neutrals
-  T oper(T a, T b) { return a + b; }
+  T f(T a, T b) { return a + b; } // operation
   T apply(T v, L l, ll s, ll e) { return v + l * (e - s); }
   // new st according to lazy
-  L prop(L a, L b) { return a + b; } // cumulative effect of lazy
+  L comb(L a, L b) { return a + b; }
+  // cumulative effect of lazy
 
   vector<T> st;
   vector<L> lazy;
   ll n;
-  STree(ll n) : st(4*n+5, tneut), lazy(4*n+5, lneut), n(n) {}
+  STree(ll n) : st(4*n, tneut), lazy(4*n, lneut), n(n) {}
   void init(ll k, ll s, ll e, const vector<T> &a) {
     lazy[k] = lneut;
     if (s + 1 == e) { st[k] = a[s]; return; }
     ll m = (s + e) / 2;
     init(2*k, s, m, a), init(2*k+1, m, e, a);
-    st[k] = oper(st[2*k], st[2*k+1]);
+    st[k] = f(st[2*k], st[2*k+1]);
   }
   void push(ll k, ll s, ll e) {
     if (lazy[k] == lneut) return; // if neutral, nothing to do
     st[k] = apply(st[k], lazy[k], s, e);
     if (s + 1 < e) { // propagate to children
-      lazy[2*k] = prop(lazy[2*k], lazy[k]);
-      lazy[2*k+1] = prop(lazy[2*k+1], lazy[k]);
+      lazy[2*k] = comb(lazy[2*k], lazy[k]);
+      lazy[2*k+1] = comb(lazy[2*k+1], lazy[k]);
     }
     lazy[k] = lneut; // clear node lazy
   }
@@ -47,21 +48,20 @@ struct STree { // example: range sum with range addition
     push(k, s, e);
     if (s >= b || e <= a) return;
     if (s >= a && e <= b) {
-      lazy[k] = prop(lazy[k], v); // accumulate lazy
+      lazy[k] = comb(lazy[k], v); // accumulate lazy
       push(k, s, e);
       return;
     }
     ll m = (s + e) / 2;
     upd(2*k, s, m, a, b, v), upd(2*k+1, m, e, a, b, v);
-    st[k] = oper(st[2*k], st[2*k+1]);
+    st[k] = f(st[2*k], st[2*k+1]);
   }
   T query(ll k, ll s, ll e, ll a, ll b) {
     if (s >= b || e <= a) return tneut;
     push(k, s, e);
     if (s >= a && e <= b) return st[k];
     ll m = (s + e) / 2;
-    return
-      oper(query(2*k, s, m, a, b), query(2*k+1, m, e, a, b));
+    return f(query(2*k, s, m, a, b),query(2*k+1, m, e, a, b));
   }
   void init(const vector<T> &a) { init(1, 0, n, a); }
   void upd(ll a, ll b, L v) { upd(1, 0, n, a, b, v); }
