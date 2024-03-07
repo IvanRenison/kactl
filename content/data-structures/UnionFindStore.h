260eabf6d352a23d3ed8880b332594cadebdb31b
Pietro Palombini <pietropalo@icloud.com>
2024-03-07 09:03:43 -0300

    Indent with tabs

--- content/data-structures/UnionFindStore.h
+++ content/data-structures/UnionFindStore.h
@@ -12,18 +12,18 @@
 
 typedef ll D;
 struct UFStore {
-    void merge(D& large, const D& small) { large += small; }
-    vi e;
-    vector<D> d;
-    UFStore(ll n) : e(n, -1), d(n) {}
-    UFStore(vector<D>& d) : e(SZ(d), -1), d(d) {}
-    D& find(ll x) { return d[repr(x)]; }
-    ll repr(ll x) { return e[x] < 0 ? x : e[x] = repr(e[x]); }
-    bool join(ll a, ll b) {
-        a = repr(a), b = repr(b);
-        if (a == b) return false;
-        if (e[a] > e[b]) swap(a, b);
-        e[a] += e[b], e[b] = a, merge(d[a], d[b]);
-        return true;
-    }
+	void merge(D& large, const D& small) { large += small; }
+	vi e;
+	vector<D> d;
+	UFStore(ll n) : e(n, -1), d(n) {}
+	UFStore(vector<D>& d) : e(SZ(d), -1), d(d) {}
+	D& find(ll x) { return d[repr(x)]; }
+	ll repr(ll x) { return e[x] < 0 ? x : e[x] = repr(e[x]); }
+	bool join(ll a, ll b) {
+		a = repr(a), b = repr(b);
+		if (a == b) return false;
+		if (e[a] > e[b]) swap(a, b);
+		e[a] += e[b], e[b] = a, merge(d[a], d[b]);
+		return true;
+	}
 };
--- test-problems/data-structures/UnionFindStore.cpp
+++ test-problems/data-structures/UnionFindStore.cpp
@@ -13,38 +13,38 @@ typedef pair<ll, ll> ii;
 typedef vector<ll> vi;
 
 struct D {
-    ll sz = 1;
+	ll sz = 1;
 };
 struct UFStore {
-    void merge(D& large, const D& small) { large.sz += small.sz; }
-    vi e;
-    vector<D> d;
-    UFStore(ll n) : e(n, -1), d(n) {}
-    UFStore(vector<D>& d) : e(SZ(d), -1), d(d) {}
-    D& find(ll x) { return d[repr(x)]; }
-    ll repr(ll x) { return e[x] < 0 ? x : e[x] = repr(e[x]); }
-    bool join(ll a, ll b) {
-        a = repr(a), b = repr(b);
-        if (a == b) return false;
-        if (e[a] > e[b]) swap(a, b);
-        e[a] += e[b], e[b] = a, merge(d[a], d[b]);
-        return true;
-    }
+	void merge(D& large, const D& small) { large.sz += small.sz; }
+	vi e;
+	vector<D> d;
+	UFStore(ll n) : e(n, -1), d(n) {}
+	UFStore(vector<D>& d) : e(SZ(d), -1), d(d) {}
+	D& find(ll x) { return d[repr(x)]; }
+	ll repr(ll x) { return e[x] < 0 ? x : e[x] = repr(e[x]); }
+	bool join(ll a, ll b) {
+		a = repr(a), b = repr(b);
+		if (a == b) return false;
+		if (e[a] > e[b]) swap(a, b);
+		e[a] += e[b], e[b] = a, merge(d[a], d[b]);
+		return true;
+	}
 };
 
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
+	cin.tie(0)->sync_with_stdio(0);
 
-    ll n, m;
-    cin >> n >> m;
-    ll largest = 1, cnt = n;
-    UFStore uf(n);
-    fore(i, 0, m) {
-        ll a, b;
-        cin >> a >> b;
-        a--, b--;
-        if (uf.join(a, b)) cnt--;
-        largest = max(largest, uf.find(a).sz);
-        cout << cnt << " " << largest << "\n";
-    }
+	ll n, m;
+	cin >> n >> m;
+	ll largest = 1, cnt = n;
+	UFStore uf(n);
+	fore(i, 0, m) {
+		ll a, b;
+		cin >> a >> b;
+		a--, b--;
+		if (uf.join(a, b)) cnt--;
+		largest = max(largest, uf.find(a).sz);
+		cout << cnt << " " << largest << "\n";
+	}
 }
