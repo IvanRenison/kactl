98e11b20e42397fd2f3db57f1ec95f96d0973f17
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-03-24 19:21:17 -0300

    Update SegmentTree2D test-problem

--- test-problems/data-structures/SegmentTree2D.cpp
+++ test-problems/data-structures/SegmentTree2D.cpp
@@ -10,49 +10,48 @@ using namespace std;
 #define ALL(x) begin(x), end(x)
 #define SZ(x) (ll)(x).size()
 #define mset(a, v) memset((a), (v), sizeof(a))
-typedef long long ll;
+typedef int ll;
 typedef pair<ll, ll> ii;
 typedef vector<ll> vi;
 
-/// content/data-structures/SegmentTree2S.cpp
+/// content/data-structures/SegmentTree2D.cpp
 struct Tree2D {
 	typedef ll T;
 	static constexpr T unit = 0;
-	T f(T a, T b) { return a + b; }
+	T f(T a, T b) { return a + b; } // associative & commutative
 
 	ll n, m;
 	vector<vi> a, st;
-	Tree2D(ll n, ll m) : n(n), m(m), a(n, vi(m)), st(2 * n, vi(2 * m)) {
-		fore(i, 0, n)
-				fore(j, 0, m)
-						st[i + n][j + m] = a[i][j];
+	Tree2D(ll n, ll m) : n(n), m(m), a(n, vi(m)),
+			st(2 * n, vi(2 * m)) {
+		fore(i, 0, n) fore(j, 0, m)
+			st[i + n][j + m] = a[i][j];
 		fore(i, 0, n) for (ll j = m - 1; j; --j)
-				st[i + n][j] = f(st[i + n][j << 1], st[i + n][j << 1 | 1]);
-		for (ll i = n - 1; i; --i)
-			fore(j, 0, 2 * m)
-					st[i][j] = f(st[i << 1][j], st[i << 1 | 1][j]);
+			st[i + n][j] = f(st[i + n][2 * j],st[i + n][2 * j + 1]);
+		for (ll i = n - 1; i; --i) fore(j, 0, 2 * m)
+			st[i][j] = f(st[2 * i][j], st[2 * i + 1][j]);
 	}
 	void upd(ll x, ll y, T v) {
 		st[x + n][y + m] = v;
-		for (ll j = y + m; j > 1; j >>= 1)
-			st[x + n][j >> 1] = f(st[x + n][j], st[x + n][j ^ 1]);
-		for (ll i = x + n; i > 1; i >>= 1)
-			for (ll j = y + m; j; j >>= 1)
-				st[i >> 1][j] = f(st[i][j], st[i ^ 1][j]);
+		for (ll j = y + m; j > 1; j /= 2)
+			st[x + n][j / 2] = f(st[x + n][j], st[x + n][j ^ 1]);
+		for (ll i = x + n; i > 1; i /= 2)
+			for (ll j = y + m; j; j /= 2)
+				st[i / 2][j] = f(st[i][j], st[i ^ 1][j]);
 	}
-	T query(ll x0, ll x1, ll y0, ll y1) {
+	T query(ll x0, ll x1, ll y0, ll y1) { // [x0, x1) * [y0, y1)
 		T r = unit;
-		for (ll i0 = x0 + n, i1 = x1 + n; i0 < i1; i0 >>= 1, i1 >>= 1) {
+		ll i0 = x0 + n, i1 = x1 + n;
+		for (; i0 < i1; i0 /= 2, i1 /= 2) {
 			ll t[4], q = 0;
-			if (i0 & 1)
-				t[q++] = i0++;
-			if (i1 & 1)
-				t[q++] = --i1;
-			fore(k, 0, q) for (ll j0 = y0 + m, j1 = y1 + m; j0 < j1; j0 >>= 1, j1 >>= 1) {
-				if (j0 & 1)
-					r = f(r, st[t[k]][j0++]);
-				if (j1 & 1)
-					r = f(r, st[t[k]][--j1]);
+			if (i0 & 1) t[q++] = i0++;
+			if (i1 & 1) t[q++] = --i1;
+			fore(k, 0, q) {
+				ll j0 = y0 + m, j1 = y1 + m;
+				for (; j0 < j1; j0 /= 2, j1 /= 2) {
+					if (j0 & 1) r = f(r, st[t[k]][j0++]);
+					if (j1 & 1) r = f(r, st[t[k]][--j1]);
+				}
 			}
 		}
 		return r;
