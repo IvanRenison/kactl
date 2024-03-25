586ca0734115052b02ad55f0f41a49b99b72e60e
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-03-25 12:11:47 -0300

    Fix `checkAllTestProblems.sh` not returning errors

--- doc/scripts/checkAllTestProblems.sh
+++ doc/scripts/checkAllTestProblems.sh
@@ -2,6 +2,6 @@
 
 for file in ./test-problems/*/*.cpp
 do
-    g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++17 $file
-    python ./doc/scripts/checkTestProblems.py $file
+    g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++17 $file || exit 1
+    python ./doc/scripts/checkTestProblems.py $file || exit 1
 done
