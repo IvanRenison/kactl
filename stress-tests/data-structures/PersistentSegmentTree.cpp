632036f5fd0f7ee69fda93e984140c25bbec7cb4
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-04-08 11:24:37 -0300

    Add persistent segment tree (#12)
    * Add `PersistentSegmentTree`
    
    * Add stress-test for `PersistentSegmentTree`
    
    * Add test problem of `PersistentSegmentTree`

---
+++ stress-tests/data-structures/PersistentSegmentTree.cpp
@@ -0,0 +1,67 @@
+#include "../utilities/template.h"
+#include "../../content/data-structures/PersistentSegmentTree.h"
+
+
+namespace Slow {
+
+#include "../../content/data-structures/SegmentTree.h"
+
+struct Slow {
+	ll n;
+	vector<Tree> trees;
+
+	Slow(ll n) : n(n), trees(1, Tree(n)) {}
+	ll upd(ll ver, ll i, Tree::T x) {
+		assert(ver < SZ(trees));
+		trees.pb(trees[ver]);
+		trees.back().update(i, x);
+		return SZ(trees) - 1;
+	}
+
+	Tree::T query(ll ver, ll l, ll r) {
+		assert(ver < SZ(trees));
+		return trees[ver].query(l, r);
+	}
+};
+
+}
+
+int main() {
+	fore(_, 0, 100) {
+		ll n = rand() % 100 + 1;
+
+		Slow::Slow slow(n);
+		STree st(n);
+
+		vi slow_versions = {0};
+		vi st_versions = {0};
+
+		fore(q, 0, 100) {
+			ll ver = rand() % SZ(slow_versions);
+
+			ll t = rand() % 2;
+
+			if (t) { // Update
+				ll i = rand() % n;
+				ll x = rand() % 10000;
+
+				slow_versions.pb(slow.upd(slow_versions[ver], i, x));
+				st_versions.pb(st.upd(st_versions[ver], i, x));
+			} else { // Query
+				ll l = rand() % n;
+				ll r = rand() % (n + 1);
+				if (l > r) swap(l, r);
+
+				ll slow_ans = slow.query(slow_versions[ver], l, r);
+				ll st_ans = st.query(st_versions[ver], l, r);
+
+				assert(slow_ans == st_ans);
+			}
+		}
+
+		assert(slow.query(slow_versions.back(), 0, n) == st.query(st_versions.back(), 0, n));
+	}
+
+	cout << "Tests passed!" << endl;
+}
+
