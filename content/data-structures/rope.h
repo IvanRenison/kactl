1ce6cba04b835309a1d8b2a2cbf9cd35bea905e6
Pietro Palombini <pietropalo@icloud.com>
2024-04-23 00:11:21 -0300

    Use spaces in rope description

--- content/data-structures/rope.h
+++ content/data-structures/rope.h
@@ -6,14 +6,14 @@
  * Description: Sequence with O(log(n)) random access, insert,
  * erase at any position. Very high constant factors.
  * Usage:
- * 	s.push_back(x);
- * 	s.insert(i,r) // insert rope r at position i
- * 	s.erase(i,k) // erase subsequence [i,i+k)
- * 	s.substr(i,k) // return a new rope
- * 	s[i] // access ith element (cannot modify)
- * 	s.mutable_reference_at(i)
- * 	s.begin() and s.end() are const iterators
- * 	s.mutable_begin(), s.mutable_end()
+ *  s.push_back(x);
+ *  s.insert(i,r) // insert rope r at position i
+ *  s.erase(i,k) // erase subsequence [i,i+k)
+ *  s.substr(i,k) // return a new rope
+ *  s[i] // access ith element (cannot modify)
+ *  s.mutable_reference_at(i)
+ *  s.begin() and s.end() are const iterators
+ *  s.mutable_begin(), s.mutable_end()
 */
 
 #include <ext/rope> /** keep-include */
