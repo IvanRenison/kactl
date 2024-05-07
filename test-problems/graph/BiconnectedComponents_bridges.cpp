74dfcf8a3701da36ff31c05adc25f59afd42ce1d
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-05-07 07:38:41 -0300

    Add test for bridges

--- test-problems/graph/BiconnectedComponents_bridges.cpp
+++ test-problems/graph/BiconnectedComponents_bridges.cpp
@@ -1,18 +1,19 @@
-/**
- * Author: Simon Lindholm
- * Date: 2017-04-17
- * License: CC0
- * Source: folklore
- * Description: Finds all biconnected components in an undirected graph, and
- *  runs a callback for the edges in each. In a biconnected component there
- *  are at least two distinct paths between any two nodes. Note that a node can
- *  be in several components. An edge which is not in a component is a bridge,
- *  i.e., not part of any cycle.
- * Time: O(E + V)
- * Status: tested with library checker ans with SPOJ EC_P
- */
-#pragma once
+// Problem: https://www.spoj.com/problems/EC_P/
+// Status: accepted
 
+#include <bits/stdc++.h>
+using namespace std;
+#define fst first
+#define snd second
+#define pb push_back
+#define fore(i, a, gmat) for (ll i = a; i < gmat; i++)
+#define ALL(x) begin(x), end(x)
+#define SZ(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+
+/// content/graph/BiconnectedComponents.h
 struct BCC_ans {
 	ll nComps; // number of biconnected components
 	vi edgesComp; // component of each edge or -1 if bridge
@@ -68,3 +69,54 @@ BCC_ans BCC(ll n, const vector<ii>& edges) {
 
 	return ans;
 };
+/// END content
+
+vector<ii> solve(ll N, const vector<ii>& edges) {
+	ll M = SZ(edges);
+
+	BCC_ans bcc = BCC(N, edges);
+
+	vector<ii> ans;
+
+	fore(e, 0, M) {
+		if (bcc.edgesComp[e] == -1) {
+			auto [a, b] = edges[e];
+			if (a > b) swap(a, b);
+			ans.push_back({a, b});
+		}
+	}
+
+	sort(ALL(ans));
+
+	return ans;
+}
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	ll TC;
+	cin >> TC;
+	fore(t, 0, TC) {
+		ll N, M;
+		cin >> N >> M;
+
+		vector<ii> edges(M);
+		for (auto& [a, b] : edges) {
+			cin >> a >> b;
+			a--, b--;
+		}
+
+		vector<ii> ans = solve(N, edges);
+
+		cout << "Caso #" << t + 1 << '\n';
+		if (ans.empty()) {
+			cout << "Sin bloqueos\n";
+		} else {
+			cout << SZ(ans) << '\n';
+			for (auto [x, y] : ans) {
+				cout << x + 1 << ' ' << y + 1 << '\n';
+			}
+		}
+	}
+}
