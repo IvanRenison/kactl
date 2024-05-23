7520a5520b7f5cb18516aac46a58835e6cbace03
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-05-23 20:47:35 -0300

    Make biconnected components a function (#15)
    * Make BiconnectedComponents a function
    * Add test problem for biconnected components
    * Add test for bridges

--- test-problems/graph/BiconnectedComponents_bridges.cpp
+++ test-problems/graph/BiconnectedComponents_bridges.cpp
@@ -1,18 +1,19 @@
-/**
- * Author: Simon Lindholm
- * Date: 2017-04-17
- * License: CC0
- * Source: folklore
- * Description: Finds all biconnected components in an undirected graph, and
- *  runs a callback for the edges in each. In a biconnected component there
- *  are at least two distinct paths between any two nodes. Note that a node can
- *  be in several components. An edge which is not in a component is a bridge,
- *  i.e., not part of any cycle.
- * Time: O(E + V)
- * Status: tested with library checker and with SPOJ EC_P
- */
-#pragma once
+// Problem: https://www.spoj.com/problems/EC_P/
+// Status: accepted
 
+#include <bits/stdc++.h>
+using namespace std;
+#define fst first
+#define snd second
+#define pb push_back
+#define fore(i, a, gmat) for (ll i = a; i < gmat; i++)
+#define ALL(x) begin(x), end(x)
+#define SZ(x) (ll)(x).size()
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+
+/// content/graph/BiconnectedComponents.h
 auto BCC(ll n, const vector<ii>& edges) {
 	ll m = SZ(edges), Time = 0, eid = 0;
 	vi num(n), st;
@@ -61,6 +62,58 @@ auto BCC(ll n, const vector<ii>& edges) {
 		nodesComp[u].insert(nComps);
 		nComps++;
 	}
-
-	return tuple(nComps, edgesComp, nodesComp);
+/// START diff
+	return tuple<ll, vi, vector<set<ll>>>(nComps, edgesComp, nodesComp);
+/// END diff
 };
+/// END content
+
+vector<ii> solve(ll N, const vector<ii>& edges) {
+	ll M = SZ(edges);
+
+	auto [nComps, edgesComp, nodesComp] = BCC(N, edges);
+
+	vector<ii> ans;
+
+	fore(e, 0, M) {
+		if (edgesComp[e] == -1) {
+			auto [a, b] = edges[e];
+			if (a > b) swap(a, b);
+			ans.push_back({a, b});
+		}
+	}
+
+	sort(ALL(ans));
+
+	return ans;
+}
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	ll TC;
+	cin >> TC;
+	fore(t, 0, TC) {
+		ll N, M;
+		cin >> N >> M;
+
+		vector<ii> edges(M);
+		for (auto& [a, b] : edges) {
+			cin >> a >> b;
+			a--, b--;
+		}
+
+		vector<ii> ans = solve(N, edges);
+
+		cout << "Caso #" << t + 1 << '\n';
+		if (ans.empty()) {
+			cout << "Sin bloqueos\n";
+		} else {
+			cout << SZ(ans) << '\n';
+			for (auto [x, y] : ans) {
+				cout << x + 1 << ' ' << y + 1 << '\n';
+			}
+		}
+	}
+}
