d2e06e049779ffcd1fe991bfcc99fe20a2e119bd
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-05-26 18:49:24 -0300

    Continue fixing link cut tree from el vasito

--- test-problems/graph/LinkCutTree.cpp
+++ test-problems/graph/LinkCutTree.cpp
@@ -43,14 +43,14 @@ struct Node_t{
 		d=N_DEL;
 	}
 	void upd();
+	ll getPV() {
+		return joinVD(tVal, dOnSeg(d, sz_));
+	}
 };
 typedef Node_t* Node;
-ll getSize(Node r){return r ? r->sz_ : 0;}
-ll getPV(Node r){
-	return r ? joinVD(r->tVal, dOnSeg(r->d,r->sz_)) : N_VAL;}
 void Node_t::upd(){
-	tVal = qOp(qOp(getPV(c[0]), joinVD(nVal, d)), getPV(c[1]));
-	sz_ = 1 + getSize(c[0]) + getSize(c[1]);
+	tVal = qOp(qOp((c[0] ? c[0]->getPV() : N_VAL), joinVD(nVal, d)), (c[1] ? c[1]->getPV() : N_VAL));
+	sz_ = 1 + (c[0] ? c[0]->sz_ : 0) + (c[1] ? c[1]->sz_ : 0);
 }
 void conn(Node c, Node p, ll il){if(c)c->p=p;if(il>=0)p->c[!il]=c;}
 void rotate(Node x){
@@ -69,11 +69,12 @@ void spa(Node x){//splay
 	}
 	x->push(); x->upd();
 }
-Node exv(Node x){//expose
-	Node last=0;
-	for(Node y=x; y; y=y->p)spa(y),y->c[0]=last,y->upd(),last=y;
-	spa(x);
-	return last;
+
+Node lift_rec(Node x, ll t) {
+	if(!x)return 0;
+	if(t==(x->c[0] ? x->c[0]->sz_ : 0)){spa(x);return x;}
+	if(t<(x->c[0] ? x->c[0]->sz_ : 0))return lift_rec(x->c[0],t);
+	return lift_rec(x->c[1],t-(x->c[0] ? x->c[0]->sz_ : 0)-1);
 }
 
 struct LinkCutTree {
@@ -91,23 +92,31 @@ struct LinkCutTree {
 		}
 	}
 
+	Node exv(ll u){ // expose
+		Node last = 0;
+		for(Node y = nodes[u]; y; y = y->p)
+			spa(y), y->c[0] = last, y->upd(), last = y;
+		spa(nodes[u]);
+		return last;
+	}
+
 	void mkR(ll u){ // makeRoot
-		exv(nodes[u]);
+		exv(u);
 		nodes[u]->rev ^= 1;
 	}
 	Node getR(ll u){
 		Node x = nodes[u];
-		exv(x);
+		exv(u);
 		while(x->c[1]) x = x->c[1];
 		spa(x);
 		return x;
 	}
 	Node lca(ll u, ll v) {
-		exv(nodes[u]);
-		return exv(nodes[v]);
+		exv(u);
+		return exv(v);
 	}
 	bool connected(ll u, ll v) {
-		exv(nodes[u]), exv(nodes[v]);
+		exv(u), exv(v);
 		return u == v ? true : nodes[u]->p != 0;
 	}
 	void link(ll u, ll v) {
@@ -115,61 +124,37 @@ struct LinkCutTree {
 		nodes[u]->p = nodes[v];
 	}
 	void cut(ll u, ll v) {
-		mkR(u), exv(nodes[v]);
+		mkR(u), exv(v);
 		nodes[v]->c[1]->p = 0, nodes[v]->c[1] = 0;
 	}
 	Node father(ll u){
-		exv(nodes[u]);
+		exv(u);
 		Node r = nodes[u]->c[1];
 		if (!r) return 0;
 		while(r->c[0]) r = r->c[0];
 		return r;
 	}
 	void cut(ll u) { // cuts x from father keeping tree root
-		exv(father(u));
+		exv(u);
 		nodes[u]->p = 0;
 	}
 	ll query(ll u, ll v) {
-		mkR(u), exv(nodes[v]);
-		return getPV(nodes[v]);
+		mkR(u), exv(v);
+		return nodes[v]->getPV();
 	}
 	void modify(ll u, ll v, ll d) {
-		mkR(u), exv(nodes[v]);
+		mkR(u), exv(v);
 		nodes[v]->d = joinD(nodes[v]->d, d);
 	}
 	ll depth(ll u) { // distance from x to its tree root
-		exv(nodes[u]);
-		return getSize(nodes[u]) - 1;
+		exv(u);
+		return nodes[u]->sz_ - 1;
+	}
+	Node lift(ll u, ll t) { // t-th ancestor of x (lift(x,1) is x's father)
+		exv(u);
+		return lift_rec(nodes[u],t);
 	}
 };
-
-void mkR(Node x){exv(x);x->rev^=1;}//makeRoot
-//Node getR(Node x){exv(x);while(x->c[1])x=x->c[1];spa(x);return x;}
-//Node lca(Node x, Node y){exv(x); return exv(y);}
-//bool connected(Node x, Node y){exv(x);exv(y); return x==y?1:x->p!=0;}
-//void link(Node x, Node y){mkR(x); x->p=y;}
-//void cut(Node x, Node y){mkR(x); exv(y); y->c[1]->p=0; y->c[1]=0;}
-// Node father(Node x){
-// 	exv(x);
-// 	Node r=x->c[1];
-// 	if(!r)return 0;
-// 	while(r->c[0])r=r->c[0];
-// 	return r;
-// }
-// void cut(Node x){ // cuts x from father keeping tree root
-// 	exv(father(x));x->p=0;}
-//ll query(Node x, Node y){mkR(x); exv(y); return getPV(y);}
-//void modify(Node x, Node y, ll d){mkR(x);exv(y);y->d=joinD(y->d,d);}
-Node lift_rec(Node x, ll t){
-	if(!x)return 0;
-	if(t==getSize(x->c[0])){spa(x);return x;}
-	if(t<getSize(x->c[0]))return lift_rec(x->c[0],t);
-	return lift_rec(x->c[1],t-getSize(x->c[0])-1);
-}
-Node lift(Node x, ll t){ // t-th ancestor of x (lift(x,1) is x's father)
-	exv(x);return lift_rec(x,t);}
-// ll depth(Node x){ // distance from x to its tree root
-// 	exv(x);return getSize(x)-1;}
 ///END content
 
 int main() {
