98eaa1aebafd710fbe9e44f4c23dcf3b072be432
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-06-30 20:22:26 -0300

    Use normal LazySegmentTree in HLD

--- content/graph/HLD.h
+++ content/graph/HLD.h
@@ -5,26 +5,26 @@
  * Source: https://codeforces.com/blog/entry/53170, https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/Trees%20(10)/HLD%20(10.3).h
  * Description: Decomposes a tree into vertex disjoint heavy paths and light
  * edges such that the path from any leaf to the root contains at most log(n)
- * light edges. Code does additive modifications and max queries, but can
+ * light edges. Code does additive modifications and sum queries, but can
  * support commutative segtree modifications/queries on paths and subtrees.
  * Takes as input the full adjacency list. VALS\_EDGES being true means that
  * values are stored in the edges, as opposed to the nodes. All values
  * initialized to the segtree default. Root must be 0.
  * Time: O((\log N)^2)
- * Status: stress-tested against old HLD
+ * Status: stress-tested a but
  */
 #pragma once
 
-#include "../data-structures/MemoryLazySegmentTree.h"
+#include "../data-structures/LazySegmentTree.h"
 
 template <bool VALS_EDGES> struct HLD {
 	ll N, tim = 0;
 	vector<vi> adj;
 	vi par, siz, depth, rt, pos;
-	Node *tree;
+	Tree t;
 	HLD(vector<vi> adj_)
 		: N(SZ(adj_)), adj(adj_), par(N, -1), siz(N, 1), depth(N),
-		  rt(N),pos(N),tree(new Node(0, N)){ dfsSz(0); dfsHld(0); }
+		  rt(N), pos(N), t(N) { dfsSz(0), dfsHld(0); }
 	void dfsSz(ll v) {
 		if (par[v] != -1) adj[v].erase(find(ALL(adj[v]), par[v]));
 		for (ll& u : adj[v]) {
@@ -49,17 +49,17 @@ template <bool VALS_EDGES> struct HLD {
 		if (depth[u] > depth[v]) swap(u, v);
 		op(pos[u] + VALS_EDGES, pos[v] + 1);
 	}
-	void modifyPath(ll u, ll v, ll val) {
-		process(u, v, [&](ll l, ll r) { tree->add(l, r, val); });
+	void updPath(ll u, ll v, L val) {
+		process(u, v, [&](ll l, ll r) { t.upd(l, r, val); });
 	}
-	ll queryPath(ll u, ll v) { // Modify depending on problem
-		ll res = -1e9;
+	T queryPath(ll u, ll v) { // Modify depending on problem
+		T res = tneut;
 		process(u, v, [&](ll l, ll r) {
-				res = max(res, tree->query(l, r));
+				res = f(res, t.query(l, r));
 		});
 		return res;
 	}
-	ll querySubtree(ll v) { // modifySubtree is similar
-		return tree->query(pos[v] + VALS_EDGES, pos[v] + siz[v]);
+	T querySubtree(ll v) { // updSubtree is similar
+		return t.query(pos[v] + VALS_EDGES, pos[v] + siz[v]);
 	}
 };
