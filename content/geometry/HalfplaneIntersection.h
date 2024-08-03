1dab130860de00a3b1fa72b20c7845e093e48c5b
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-08-03 17:19:03 -0300

    Improve Description

--- content/geometry/HalfplaneIntersection.h
+++ content/geometry/HalfplaneIntersection.h
@@ -4,9 +4,9 @@
  * License: CC0
  * Source: https://github.com/zimpha/algorithmic-library/blob/master/computational-geometry/polygon.cc
  * Description: Computes the intersection of a set of half-planes. Input is given as a set of planes, facing left.
- * Output is the convex polygon representing the intersection. The points may have duplicates and be collinear.
- * Will not fail catastrophically if `eps > sqrt(2)(line intersection error)`. Likely to work for more ranges if
- * 3 half planes are never guaranteed to intersect at the same point.
+ * The intersection must form a convex polygon or be empty. Output is the convex polygon representing the intersection.
+ * The points may have duplicates and be collinear. Will not fail catastrophically if `eps > sqrt(2)(line intersection error)`.
+ * Likely to work for more ranges if 3 half planes are guaranteed to never intersect at the same point.
  * Time: O(n \log n)
  * Status: stress-tested
  */
