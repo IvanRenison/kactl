3b770f695db751a2cccbd55e2d3315ad10c7e11e
Pietro Palombini <pietropalo@icloud.com>
2024-08-07 22:40:38 -0300

    Delete old macros

--- stress-tests/utilities/template.h
+++ stress-tests/utilities/template.h
@@ -4,15 +4,11 @@ using namespace std;
 #define fst first
 #define snd second
 #define pb push_back
-#define rep(i, a, b) for (ll i = a; i < (b); i++)
 #define fore(i, a, b) for (ll i = a, gmat = b; i < gmat; i++)
-#define all(x) x.begin(), x.end()
-#define sz(x) (ll)(x).size()
 #define ALL(x) begin(x), end(x)
 #define SZ(x) (ll)(x).size()
 #define mset(a, v) memset((a), (v), sizeof(a))
 
 typedef long long ll;
 typedef pair<ll, ll> ii;
-typedef pair<ll, ll> pii;
 typedef vector<ll> vi;
