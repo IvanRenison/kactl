45e2b6f6e9cab3081901b67c3fa13482f4168843
Pietro Palombini <pietropalo@icloud.com>
2024-08-07 22:38:05 -0300

    Change utilities macros

--- stress-tests/utilities/genPolygon.h
+++ stress-tests/utilities/genPolygon.h
@@ -10,13 +10,13 @@ template<class P> pair<bool, vector<P>> conquer(vector<P> pts, ll depth) {
 	if (depth>100) {
 		return {false, {}};
 	}
-	if (sz(pts) <= 2) return {true,pts};
-	if (sz(pts) == 3) {
+	if (SZ(pts) <= 2) return {true,pts};
+	if (SZ(pts) == 3) {
 		swap(pts[1], pts[2]);
 		return {true,pts};
 	}
 
-	ll divideId = randRange(2, sz(pts));
+	ll divideId = randRange(2, SZ(pts));
 	P p1 = pts[divideId];
 	double divideK = randDouble(0.01, 0.99);
 	P p2(divideK*(pts[1].x-pts[0].x) + pts[0].x, divideK*(pts[1].y - pts[0].y) + pts[0].y);
@@ -26,47 +26,47 @@ template<class P> pair<bool, vector<P>> conquer(vector<P> pts, ll depth) {
 	if (idx0==idx1)
 		return conquer(pts, depth+1);
 	array<vector<P>, 2> S;
-	S[idx0].push_back(pts[0]);
-	S[idx0].push_back(p1);
-	S[!idx0].push_back(p1);
-	S[!idx0].push_back(pts[1]);
-	rep(i,2,sz(pts)) {
+	S[idx0].pb(pts[0]);
+	S[idx0].pb(p1);
+	S[!idx0].pb(p1);
+	S[!idx0].pb(pts[1]);
+	fore(i,2,SZ(pts)) {
 		if (i == divideId) continue;
 		ll idx = ((line[0]*pts[i].x + line[1]*pts[i].y + line[2]) >=0);
-		S[idx].push_back(pts[i]);
+		S[idx].pb(pts[i]);
 	}
 	auto pa = conquer(S[idx0], depth+1);
 	auto pb = conquer(S[!idx0], depth+1);
-	if (!pa.first || !pb.first) return {false, {}};
-	pb.second.erase(pb.second.begin());
-	pa.second.insert(pa.second.end(), all(pb.second));
+	if (!pa.fst || !pb.fst) return {false, {}};
+	pb.snd.erase(pb.snd.begin());
+	pa.snd.insert(pa.snd.end(), ALL(pb.snd));
 	return pa;
 }
 template<class P> vector<P> genPolygon(vector<P> pts, ll depth=0) {
 	if (depth>100) return {P(0,0), P(1,0), P(0,1)};
-	sort(all(pts));
-	pts.resize(unique(all(pts)) - pts.begin());
+	sort(ALL(pts));
+	pts.resize(unique(ALL(pts)) - pts.begin());
 	shuffle_vec(pts);
-	if (sz(pts) <=3) return pts;
+	if (SZ(pts) <=3) return pts;
 	vector<double> line ={(double)(pts[1].y-pts[0].y), (double)(pts[0].x - pts[1].x), (double)(-pts[0].x*pts[1].y + pts[0].y*pts[1].x)};
 	array<vector<P>, 2> S;
-	S[0].push_back(pts[0]);
-	S[0].push_back(pts[1]);
-	S[1].push_back(pts[1]);
-	S[1].push_back(pts[0]);
-	rep(i,2,sz(pts)) {
+	S[0].pb(pts[0]);
+	S[0].pb(pts[1]);
+	S[1].pb(pts[1]);
+	S[1].pb(pts[0]);
+	fore(i,2,SZ(pts)) {
 		ll idx = (line[0]*pts[i].x + line[1]*pts[i].y + line[2]) >=0;
-		S[idx].push_back(pts[i]);
+		S[idx].pb(pts[i]);
 	}
 	auto ta = conquer(S[0],0);
 	auto tb = conquer(S[1],0);
-	auto pa=ta.second, pb=tb.second;
-	if (!ta.first || !tb.first) {
+	auto pa=ta.snd, pb=tb.snd;
+	if (!ta.fst || !tb.fst) {
 		return genPolygon(pts, depth+1);
 	}
 	pa.erase(pa.begin());
 	pb.erase(pb.begin());
-	pa.insert(pa.end(), all(pb));
-	if (polygonArea2(pa) < 0) reverse(all(pa));
+	pa.insert(pa.end(), ALL(pb));
+	if (polygonArea2(pa) < 0) reverse(ALL(pa));
 	return pa;
 }
