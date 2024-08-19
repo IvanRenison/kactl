5a7b6641cd92f4219cc6434a5b10f6a5467eac46
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-08-19 09:24:19 -0300

    Bench queries

--- stress-tests/data-structures/FastRMQ.cpp
+++ stress-tests/data-structures/FastRMQ.cpp
@@ -24,7 +24,7 @@ void testCase() {
 	}
 }
 
-void bench() {
+void bench_construction() {
 	for (ll n = 10; n <= 1e7; n *= 10) {
 		vi V(n);
 		fore(i, 0, n) {
@@ -48,12 +48,54 @@ void bench() {
 	}
 }
 
+void bench_queries() {
+	for (ll n = 10; n <= 1e7; n *= 10) {
+		vi V(n);
+		fore(i, 0, n) {
+			V[i] = rand() % 1000;
+		}
+
+		vector<ii> queries(1e6);
+		fore(i, 0, 1e6) {
+			ll b = rand() % n + 1;
+			ll a = rand() % b;
+			queries[i] = {a, b};
+		}
+
+		RMQ<ll> rmq(V);
+		ll x0 = 0;
+		auto start = chrono::high_resolution_clock::now();
+		for (auto [a, b] : queries) {
+			x0 ^= rmq.query(a, b);
+		}
+		auto end = chrono::high_resolution_clock::now();
+
+		ll time = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+		nlog::RMQ<ll> rmq_nlog(V);
+		start = chrono::high_resolution_clock::now();
+		for (auto [a, b] : queries) {
+			x0 ^= rmq_nlog.query(a, b);
+		}
+		end = chrono::high_resolution_clock::now();
+
+		assert(x0 == 0);
+
+		ll time_nlog = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+		cout << "n = " << n << ", On: " << time << "ms, Onlogn: " << time_nlog << "ms" << endl;
+		assert(time >= time_nlog);
+	}
+}
+
+
 int main() {
 	fore(_, 0, 1000) {
 		testCase();
 	}
 
-	bench();
+	bench_construction();
+	bench_queries();
 
 	cout << "Tests passed!" << endl;
 }
