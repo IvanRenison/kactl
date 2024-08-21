52fd96eb14916662d0dfccb83685f31a59be2d1e
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-08-21 16:06:20 -0300

    Use C++ 20 (#48)

--- doc/scripts/checkAllTestProblems.sh
+++ doc/scripts/checkAllTestProblems.sh
@@ -4,6 +4,6 @@ set -e
 
 for file in ./test-problems/*/*.cpp
 do
-    g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++17 $file
+    g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++20 $file
     python3 ./doc/scripts/checkTestProblems.py $file
 done
