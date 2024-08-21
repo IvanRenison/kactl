cb56b27b6af98a1d5620d3f0e2e677e1c7ab3761
Pietro Palombini <pietropalo@icloud.com>
2024-08-21 12:56:09 -0300

    Add FastInverse

---
+++ content/number-theory/FastInverse.h
@@ -0,0 +1,20 @@
+/**
+ * Author: Sergey Slotin
+ * Date: 2024-08-21
+ * Description: Fast modular inverse for a constant modulus.
+ * Time: O(log n), $\approx$ 2.7x faster than euclid in CF.
+ * Status: stress-tested
+ */
+#pragma once
+
+constexpr ll mod = 1e9 + 7;
+constexpr ll k = bit_width((unsigned long long)(mod - 2));
+ll inv(ll a) {
+    ll r = 1;
+#pragma GCC unroll(k)
+    fore(l, 0, k) {
+        if ((mod - 2) >> l & 1) r = r * a % mod;
+        a = a * a % mod;
+    }
+    return r;
+}
---
+++ stress-tests/number-theory/FastInverse.cpp
@@ -0,0 +1,14 @@
+#include "../utilities/template.h"
+#include "../../content/number-theory/FastInverse.h"
+
+int main() {
+    srand(2);
+
+    fore(_, 0, 10000000) {
+        ll a = rand() % (mod - 1) + 1;
+        ll ainv = inv(a);
+        assert((a * ainv) % mod == 1);
+    }
+
+    cout << "Tests passed!" << endl;
+}
