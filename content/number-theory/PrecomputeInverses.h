89eed506e03c336ccaa95355e13f7339a2232dc7
Pietro Palombini <52302877+Aplietexe@users.noreply.github.com>
2024-08-21 18:47:27 -0300

    Use function for ModInverse (#41)
    * Update ModInverse
    
    * Remove unnecessary parentheses

--- content/number-theory/PrecomputeInverses.h
+++ content/number-theory/PrecomputeInverses.h
@@ -8,6 +8,9 @@
  */
 #pragma once
 
-// const ll mod = 1000000007, LIM = 200000; ///include-line
-ll* inv = new ll[LIM] - 1; inv[1] = 1;
-fore(i,2,LIM) inv[i] = mod - (mod / i) * inv[mod % i] % mod;
+constexpr ll mod = 1e9+7, LIM = 2e5;
+array<ll, LIM> inv;
+void initInv() {
+	inv[1] = 1;
+	fore(i,2,LIM) inv[i] = mod - mod / i * inv[mod % i] % mod;
+}
