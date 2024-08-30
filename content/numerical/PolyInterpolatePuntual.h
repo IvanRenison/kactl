32410ef8990ed13b9c6562f2af19e1168ab494d3
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-08-30 15:43:36 -0300

    Add `PolyInterpolatePuntual`

---
+++ content/numerical/PolyInterpolatePuntual.h
@@ -0,0 +1,29 @@
+/**
+ * Author: Iván Renison
+ * Date: 2024-08-30
+ * License: CC0
+ * Source: me
+ * Description: Given for $i \in \{0, 1, ..., n-1\} $ points $(i, y[i])$ computes de value
+ * of the n-1-degree polynomial that passes through them at point $x$.
+ * Time: O(n \log mod)
+ * status: problem tested
+ */
+#pragma once
+
+#include "ModularArithmetic.h"
+
+Mod interpolate(vector<Mod>& y, Mod x) {
+	ll n = SZ(y);
+	static vector<Mod> fi {1}; // Inverses of factorials
+	while (SZ(fi) < n) fi.pb(fi.back() / SZ(fi));
+	if (x.x < n) return y[x.x];
+	Mod p = 1;
+	fore(i, 0, n) p = p * (x - i);
+	Mod ans = 0;
+	fore(i, 0, n) {
+		Mod t = y[i] * p / (x-i) * fi[i] * fi[n-1-i];
+		if ((n-i) % 2 == 0) t = t * (mod - 1);
+		ans = ans + t;
+	}
+	return ans;
+}
---
+++ test-problems/numerical/PolyInterpolatePuntual.cpp
@@ -0,0 +1,83 @@
+// Problem: https://codeforces.com/contest/622/problem/F
+// Status: Accepted
+// Submission: https://codeforces.com/contest/622/submission/278851327
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fst first
+#define snd second
+#define pb push_back
+#define fore(i, a, b) for (ll i = a, gmat = b; i < gmat; i++)
+#define ALL(x) begin(x), end(x)
+#define SZ(x) (ll)(x).size()
+#define mset(a, v) memset((a), (v), sizeof(a))
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+
+/// content/number-theory/euclid.h
+ll euclid(ll a, ll b, ll &x, ll &y) {
+	if (!b) return x = 1, y = 0, a;
+	ll d = euclid(b, a % b, y, x);
+	return y -= a/b * x, d;
+}
+/// END content
+
+/// content/number-theory/ModularArithmetic.h
+/// START diff
+const ll mod = 1e9 + 7;
+/// END diff
+struct Mod {
+	ll x;
+	Mod(ll xx) : x(xx) {}
+	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
+	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
+	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
+	Mod operator/(Mod b) { return *this * invert(b); }
+	Mod invert(Mod a) {
+		ll x, y, g = euclid(a.x, mod, x, y);
+		assert(g == 1); return Mod((x + mod) % mod);
+	}
+	Mod operator^(ll e) {
+		if (!e) return Mod(1);
+		Mod r = *this ^ (e / 2); r = r * r;
+		return e&1 ? *this * r : r;
+	}
+};
+/// END content
+
+/// content/numerical/PolyInterpolatePuntual.h
+Mod interpolate(vector<Mod>& y, Mod x) {
+	ll n = SZ(y);
+	static vector<Mod> fi {1}; // Inverses of factorials
+	while (SZ(fi) < n) fi.pb(fi.back() / SZ(fi));
+	if (x.x < n) return y[x.x];
+	Mod p = 1;
+	fore(i, 0, n) p = p * (x - i);
+	Mod ans = 0;
+	fore(i, 0, n) {
+		Mod t = y[i] * p / (x-i) * fi[i] * fi[n-1-i];
+		if ((n-i) % 2 == 0) t = t * (mod - 1);
+		ans = ans + t;
+	}
+	return ans;
+}
+/// END content
+
+Mod solve(ll n, ll k) {
+	vector<Mod> y = {0};
+	fore(i, 1, k + 2) {
+		y.pb(y.back() + (Mod(i) ^ k));
+	}
+	Mod ans = interpolate(y, Mod(n));
+	return ans;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	ll n, k;
+	cin >> n >> k;
+	Mod ans = solve(n, k);
+	cout << ans.x << '\n';
+}
