134d6b15ba9bc4106a5a4e24238ba15346cf41fa
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-09-08 20:05:38 -0300

    Improve polynomial

--- content/numerical/Polynomial.h
+++ content/numerical/Polynomial.h
@@ -1,24 +1,42 @@
 /**
- * Author: David Rydh, Per Austrin
- * Date: 2003-03-16
- * Description:
+ * Author: Iván Renison
+ * Date: 2024-09-08
+ * Source: Original from kactl and notebook el vasito
+ * Description: Some operations with polynomials. Everything except \texttt{div} works
+ * also with integers.
+ * Time: All O(n) except for \texttt{div} witch is O(n^2)
+ * Status: untested
  */
 #pragma once
 
-struct Poly {
-	vector<double> a;
-	double operator()(double x) const {
-		double val = 0;
-		for (ll i = SZ(a); i--;) (val *= x) += a[i];
-		return val;
+const double eps = 1e-9;
+typedef vector<double> Poly;
+
+double eval(const Poly& p, double x) {
+	double val = 0;
+	for (ll i = SZ(p); i--;) (val *= x) += p[i];
+	return val;
+}
+Poly derivate(const Poly& p) {
+	Poly res(SZ(p)-1);
+	fore(i, 1, SZ(p)) res[i-1] = i*p[i];
+	return res;
+}
+pair<Poly, double> divroot(const Poly& p, double x0) {
+	ll n = SZ(p) - 1;            // Divide p by (x-x0)
+	Poly res(n);
+	res[n-1] = p[n];
+	for (ll i = n - 1; i--; ) res[i] = p[i+1] + x0 * res[i+1];
+	return {res, p[0] + x0 * res[0]};
+}
+pair<Poly, Poly> div(Poly p, const Poly& q) {
+	ll n= SZ(p) - SZ(q) + 1;
+	Poly res(n);
+	for (ll i = n; i--; ){
+		res[i] = p.back() / q.back();
+		fore(j, 0, SZ(q)) p[j + i] -= res[i] * q[j];
+		p.pop_back();
 	}
-	void diff() {
-		fore(i,1,SZ(a)) a[i-1] = i*a[i];
-		a.pop_back();
-	}
-	void divroot(double x0) {
-		double b = a.back(), c; a.back() = 0;
-		for(ll i=SZ(a)-1; i--;) c = a[i], a[i] = a[i+1]*x0+b, b=c;
-		a.pop_back();
-	}
-};
+	while (!p.empty() && abs(p.back()) < eps) p.pop_back();
+	return {res, p};
+}
