ac3d3fbf7e14569e5006e17c591d21c302e1192f
Pietro Palombini <pietropalo@icloud.com>
2024-10-11 19:49:13 -0300

    Update ModularArithmetic

--- content/number-theory/ModularArithmetic.h
+++ content/number-theory/ModularArithmetic.h
@@ -3,14 +3,14 @@
  * Date: 2009-09-28
  * License: CC0
  * Source: folklore
- * Description: Operators for modular arithmetic. You need to set {\tt mod} to
- * some number first and then you can use the structure.
+ * Description: Operators for modular arithmetic. Update mod.
+ * Use commented code in invert if mod is not prime.
  */
 #pragma once
 
 #include "euclid.h"
 
-const ll mod = 17; // change to something else
+const ll mod = 17;
 struct Mod {
 	ll x;
 	Mod(ll xx) : x(xx) {}
@@ -19,12 +19,14 @@ struct Mod {
 	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
 	Mod operator/(Mod b) { return *this * invert(b); }
 	Mod invert(Mod a) {
-		ll x, y, g = euclid(a.x, mod, x, y);
-		assert(g == 1); return Mod((x + mod) % mod);
+		return a ^ (mod - 2);
+		// ll x, y, g = euclid(a.x, mod, x, y);
+		// assert(g == 1); return Mod((x + mod) % mod);
 	}
 	Mod operator^(ll e) {
-		if (!e) return Mod(1);
-		Mod r = *this ^ (e / 2); r = r * r;
-		return e&1 ? *this * r : r;
+		Mod ans(1);
+		for (Mod b = *this; e; b = b * b, e >> 1)
+			if (e & 1) ans = ans * b;
+		return ans;
 	}
 };
