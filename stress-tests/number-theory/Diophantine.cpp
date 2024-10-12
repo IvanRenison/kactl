bfdf7defaf57ab3822ea02826a987ead8dee456e
Pietro Palombini <pietropalo@icloud.com>
2024-10-11 21:29:13 -0300

    Use tabs in test

--- stress-tests/number-theory/Diophantine.cpp
+++ stress-tests/number-theory/Diophantine.cpp
@@ -4,13 +4,13 @@
 #include "../../content/various/Randin.h"
 
 int main() {
-  ll n = 1e7, mx = 1e9;
-  fore(_, 0, n) {
-    ll a = randin(-mx, mx+1), b = randin(-mx, mx+1), k = randin(-mx, mx+1);
-    ll r = gcd(a, b) * k;
-    auto [x, y, dx, dy] = diophantine(a, b, r);
-    assert(a * x + b * y == r);
-    fore(i, -15, 16)
-      assert(a * (x + i * dx) + b * (y + i * dy) == r);
-  }
+	ll n = 1e7, mx = 1e9;
+	fore(_, 0, n) {
+		ll a = randin(-mx, mx+1), b = randin(-mx, mx+1), k = randin(-mx, mx+1);
+		ll r = gcd(a, b) * k;
+		auto [x, y, dx, dy] = diophantine(a, b, r);
+		assert(a * x + b * y == r);
+		fore(i, -15, 16)
+			assert(a * (x + i * dx) + b * (y + i * dy) == r);
+	}
 }
