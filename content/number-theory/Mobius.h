81467f362bc5a627cb02d4aadd98ea15d01b90d3
Pietro Palombini <52302877+Aplietexe@users.noreply.github.com>
2024-10-13 13:52:20 -0300

    Add Mobius.h (#69)

---
+++ content/number-theory/Mobius.h
@@ -0,0 +1,17 @@
+/**
+ * Author: Unknown
+ * Date: 2024-10-13
+ * Description: Computes the Mobius function $\mu(n)$ for all
+ * $n < L$.
+ * Time: O(L \log L)
+ * Status: stress-tested
+ */
+
+#pragma once
+
+const ll L = 1e6;
+array<int8_t, L> mu;
+void calculateMu() {
+	mu[1] = 1;
+	fore(i,1,L) if(mu[i]) for(ll j=2*i; j<L; j+=i) mu[j]-=mu[i];
+}
