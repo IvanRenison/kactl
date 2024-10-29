afc1f844add72a1b8cfe4d40c2f0db9e7c299123
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-10-29 11:08:24 -0300

    Fix description of MinkowskiSum (#84)

--- content/geometry/MinkowskiSum.h
+++ content/geometry/MinkowskiSum.h
@@ -5,7 +5,7 @@
  * Source: notebook el vasito
  * Description: Compute Minkowski sum of two strictly convex non empty polygons (i.e. two hulls).
  * Returns answer in CCW order.
- * The Minkowski sum of two polygons $$P$$ and $$Q$$ viewed as sets of $\mathbb{R}^2$ is defined as
+ * The Minkowski sum of two polygons $P$ and $Q$ viewed as sets of $\mathbb{R}^2$ is defined as
  * $\{p + q : p \in P, q \in Q\}$
  * Time: O(n \log n + m \log m)
  * Status: stress-tested
