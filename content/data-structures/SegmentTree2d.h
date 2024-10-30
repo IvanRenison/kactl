4875d821f7a8d9b5d2a727a0d571c6055e4f6c12
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-10-30 16:31:41 -0300

    Shorten SegmentTree2d (#87)
    * Shorten SegmentTree2d
    
    * Fix

--- content/data-structures/SegmentTree2d.h
+++ content/data-structures/SegmentTree2d.h
@@ -20,35 +20,30 @@ struct Tree2 {
 	vector<vector<T>> a, st;
 	Tree2(ll n, ll m) : n(n), m(m), a(n, vector<T>(m)),
 			st(2 * n, vector<T>(2 * m)) {
-		fore(i, 0, n) fore(j, 0, m)
-			st[i + n][j + m] = a[i][j];
-		fore(i, 0, n) for (ll j = m - 1; j; --j)
-			st[i + n][j] = f(st[i + n][2 * j],st[i + n][2 * j + 1]);
-		for (ll i = n - 1; i; --i) fore(j, 0, 2 * m)
+		fore(i, 0, n) fore(j, 0, m) st[i + n][j + m] = a[i][j];
+		fore(i, 0, n) for (ll j = m; --j;)
+			st[i + n][j] = f(st[i + n][2 * j], st[i + n][2 * j + 1]);
+		for (ll i = n; --i;) fore(j, 0, 2 * m)
 			st[i][j] = f(st[2 * i][j], st[2 * i + 1][j]);
 	}
 	void upd(ll x, ll y, T v) {
 		st[x + n][y + m] = v;
 		for (ll j = y + m; j > 1; j /= 2)
 			st[x + n][j / 2] = f(st[x + n][j], st[x + n][j ^ 1]);
-		for (ll i = x + n; i > 1; i /= 2)
-			for (ll j = y + m; j; j /= 2)
-				st[i / 2][j] = f(st[i][j], st[i ^ 1][j]);
+		for (x += n; x > 1; x /= 2) for (ll j = y + m; j; j /= 2)
+			st[x / 2][j] = f(st[x][j], st[x ^ 1][j]);
 	}
 	T query(ll x0, ll x1, ll y0, ll y1) { // [x0, x1) * [y0, y1)
 		T r = neut;
-		ll i0 = x0 + n, i1 = x1 + n;
-		for (; i0 < i1; i0 /= 2, i1 /= 2) {
+		for (x0 += n, x1 += n; x0 < x1; x0 /= 2, x1 /= 2) {
 			ll t[4], q = 0;
-			if (i0 & 1) t[q++] = i0++;
-			if (i1 & 1) t[q++] = --i1;
-			fore(k, 0, q) {
-				ll j0 = y0 + m, j1 = y1 + m;
-				for (; j0 < j1; j0 /= 2, j1 /= 2) {
+			if (x0 & 1) t[q++] = x0++;
+			if (x1 & 1) t[q++] = --x1;
+			fore(k, 0, q)
+				for (ll j0 = y0+m, j1 = y1+m; j0 < j1; j0/=2, j1/=2) {
 					if (j0 & 1) r = f(r, st[t[k]][j0++]);
 					if (j1 & 1) r = f(r, st[t[k]][--j1]);
 				}
-			}
 		}
 		return r;
 	}
