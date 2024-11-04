be41a5fcb161e1df390aa102f74509059a6e32c3
Pietro Palombini <pietropalo@icloud.com>
2024-11-04 18:13:57 -0300

    Add new MoQueries and stress-test

--- stress-tests/data-structures/MoQueries.cpp
+++ stress-tests/data-structures/MoQueries.cpp
@@ -1,181 +1,204 @@
 #include "../utilities/template.h"
 
-// #include "../../content/data-structures/MoQueries.h"
+#include "../../content/various/Randin.h"
 
-ll curL = 0, curR = 0, ops = 0;
-void add(ll ind, ll end) {
-	if (curL != curR) {
-		if (end == 0) assert(ind == curL - 1);
-		else assert(ind == curR);
+/// content/strings/Hashing.h
+// Arithmetic mod 2^64-1. 2x slower than mod 2^64 and more
+// code, but works on evil test data (e.g. Thue-Morse, where
+// ABBA... and BAAB... of length 2^10 hash the same mod 2^64).
+// "typedef ull H;" instead if you think test data is random,
+// or work mod 10^9+7 if the Birthday paradox is not a problem.
+typedef uint64_t ull;
+/// START diff
+typedef ull H;
+/// END diff
+static const H C = (ll)1e11+3; // (order ~ 3e9; random also ok)
+
+struct HashInterval {
+	vector<H> ha, pw;
+	/// START diff
+	HashInterval(vi& str) : ha(SZ(str)+1), pw(ha) {
+	/// END diff
+		pw[0] = 1;
+		fore(i,0,SZ(str))
+			ha[i+1] = ha[i] * C + str[i],
+			pw[i+1] = pw[i] * C;
 	}
-	if (curL == curR) curL = ind, curR = ind + 1;
-	else if (ind == curR) curR++;
-	else curL--;
-	ops++;
-}
-void del(ll ind, ll end) {
-	if (end == 0) assert(ind == curL);
-	else assert(ind == curR - 1);
-	if (ind == curR - 1) curR--;
-	else curL++;
-	assert(curL <= curR);
-	ops++;
-}
-
-ll calc() {
-	return curL == curR ? -1 : curL + (curR - curL) * 10;
-}
-
-ll blk; // ~N/sqrt(Q)
-vi mo(vector<ii> Q) {
-	ll L = 0, R = 0;
-	vi s(SZ(Q)), res = s;
-#define K(x) ii(x.fst/blk, x.snd ^ -(x.fst/blk & 1))
-	iota(ALL(s), 0);
-	sort(ALL(s), [&](ll s, ll t) { return K(Q[s]) < K(Q[t]); });
-	for (ll qi : s) {
-		ii q = Q[qi];
-		while (L > q.fst) add(--L, 0);
-		while (R < q.snd) add(R++, 1);
-		while (L < q.fst) del(L++, 0);
-		while (R > q.snd) del(--R, 1);
-		res[qi] = calc();
+	H hashInterval(ll a, ll b) { // hash [a, b)
+		return ha[b] - ha[a] * pw[b - a];
 	}
-	return res;
+};
+/// START diff
+/// END diff
+/// END content
+
+const ull invC = 18245175647506858667ULL;
+
+namespace SQRT {
+/// content/data-structures/MoQueries.h
+/// START diff
+ll B = 447; // ~N/sqrt(Q)
+/// END diff
+struct MoQueries {
+	/// START diff
+  typedef H T;
+	vi& a;
+	vector<H>& pws;
+	H ans = 0;
+	ll sz = 0;
+  void add(ll pos, bool end) {
+		if (end)
+			ans = ans * C + a[pos];
+		else
+			ans = pws[sz] * a[pos] + ans;
+		sz++;
+	}
+  void rem(ll pos, bool end) {
+		sz--;
+		if (end)
+			ans = (ans - a[pos]) * invC;
+		else
+			ans = ans - pws[sz] * a[pos];
+	}
+  T calc() { return ans; }
+	/// END diff
+  ii k(ii &x){return ii(x.fst/B, x.snd ^ -(x.fst/B&1));}// SQ
+  vector<T> solve(ll n, vector<ii> &qs) {
+    ll l=0,r=0,q=SZ(qs); //,k=bit_width((unsigned)n), rx,ry;
+    vi p(q); //,o(q);
+    iota(ALL(p), 0);
+  //fore(i, 0, q) {
+  //  ll x = qs[i].fst, y = qs[i].snd;
+  //  for (ll s = k; --s;) {
+  //    rx=(x>>s)&1, ry=(y>>s)&1, o[i] += ((rx*3)^ry) << (2*s);
+  //    if (!ry) {
+  //      if (rx) x = (1 << k) - 1 - x, y = (1 << k) - 1 - y;
+  //      swap(x, y);
+  //    }
+  //  }
+  //}
+  //sort(ALL(p), [&](ll i, ll j) { return o[i] < o[j]; });
+    sort(ALL(p),[&](ll i,ll j){return k(qs[i])<k(qs[j]);});//SQ
+    vector<T> res(q);
+    for (ll i : p) {
+        auto [ql, qr] = qs[i];
+        while (l > ql) add(--l, 0);
+        while (r < qr) add(r++, 1);
+        while (l < ql) rem(l++, 0);
+        while (r > qr) rem(--r, 1);
+        res[i] = calc();
+    }
+    return res;
+}
+};
+/// END content
 }
 
-void test(ll n, ll q) {
-	curL = curR = ops = 0;
-	blk = max((ll)(n / sqrt(max(q, 1ll))), 1ll);
-	vector<ii> queries(q);
-	for (auto& pa : queries) {
-		pa.fst = rand() % n;
-		pa.snd = rand() % n;
-		if (pa.fst > pa.snd)
-			swap(pa.fst, pa.snd);
+namespace Hilbert {
+/// content/data-structures/MoQueries.h
+/// START diff
+ll B = 447; // ~N/sqrt(Q)
+/// END diff
+struct MoQueries {
+	/// START diff
+  typedef H T;
+	vi& a;
+	vector<H>& pws;
+	H ans = 0;
+	ll sz = 0;
+  void add(ll pos, bool end) {
+		if (end)
+			ans = ans * C + a[pos];
+		else
+			ans = pws[sz] * a[pos] + ans;
+		sz++;
 	}
-	vi res = mo(queries);
-	fore(i,0,q) {
-		ll l = queries[i].fst, r = queries[i].snd;
-		if (l == r) {
-			assert(res[i] == -1);
-		} else {
-			assert(res[i] == l + (r - l) * 10);
-		}
+  void rem(ll pos, bool end) {
+		sz--;
+		if (end)
+			ans = (ans - a[pos]) * invC;
+		else
+			ans = ans - pws[sz] * a[pos];
 	}
-	// (This inequality holds for random queries; in general it's off by a small constant)
-	if (n > 100 && q > 100) {
-		// cout << n << ' ' << q << ' ' << ops / (n * sqrt(q)) << endl;
-		assert(ops < n * sqrt(q));
-	}
+  T calc() { return ans; }
+  // ii k(ii &x){return ii(x.fst/B, x.snd ^ -(x.fst/B&1));}// SQ
+	/// END diff
+  vector<T> solve(ll n, vector<ii> &qs) {
+		/// START diff
+    ll l=0,r=0,q=SZ(qs),k=bit_width((unsigned)n), rx,ry;
+    vi p(q),o(q);
+		/// END diff
+    iota(ALL(p), 0);
+		/// START diff
+    fore(i, 0, q) {
+      ll x = qs[i].fst, y = qs[i].snd;
+      for (ll s = k; --s;) {
+        rx=(x>>s)&1, ry=(y>>s)&1, o[i] += ((rx*3)^ry) << (2*s);
+        if (!ry) {
+          if (rx) x = (1 << k) - 1 - x, y = (1 << k) - 1 - y;
+          swap(x, y);
+        }
+      }
+    }
+    sort(ALL(p), [&](ll i, ll j) { return o[i] < o[j]; });
+    // sort(ALL(p),[&](ll i,ll j){return k(qs[i])<k(qs[j]);});//SQ
+		/// END diff
+    vector<T> res(q);
+    for (ll i : p) {
+        auto [ql, qr] = qs[i];
+        while (l > ql) add(--l, 0);
+        while (r < qr) add(r++, 1);
+        while (l < ql) rem(l++, 0);
+        while (r > qr) rem(--r, 1);
+        res[i] = calc();
+    }
+    return res;
+}
+};
+/// END content
 }
 
-#undef K
-
-namespace MoTree {
-
-vi vals;
-ll sum;
-deque<ll> path;
-void add(ll i, ll end) {
-	sum += vals[i];
-	ops++;
-	if (end == 0) path.push_front(i);
-	else path.pb(i);
-}
-void del(ll i, ll end) {
-	sum -= vals[i];
-	ops++;
-	assert(!path.empty());
-	if (end == 0) {
-		assert(path.front() == i);
-		path.pop_front();
-	} else {
-		assert(path.back() == i);
-		path.pop_back();
+void test(ll n, ll q, vector<H> &pws) {
+	vi a(n);
+	fore(i, 0, n) a[i] = randin(0ll, (ll)1e9);
+	vector<ii> qs(q);
+	fore(i, 0, q) {
+		ll l = randin(0ll, n), r = randin(l, n+1);
+		qs[i] = {l, r};
 	}
-}
-ll calc() { return sum; }
 
-vi moTree(vector<array<ll, 2>> Q, vector<vi>& ed, ll root=0) {
-	ll N = SZ(ed), pos[2] = {};
-	vi s(SZ(Q)), res = s, I(N), L(N), R(N), in(N), par(N);
-	add(0, 0), in[0] = 1;
-	auto dfs = [&](ll x, ll p, ll dep, auto&& f) -> void {
-		par[x] = p;
-		L[x] = N;
-		if (dep) I[x] = N++;
-		for (ll y : ed[x]) if (y != p) f(y, x, !dep, f);
-		if (!dep) I[x] = N++;
-		R[x] = N;
-	};
-	dfs(root, -1, 0, dfs);
-#define K(x) ii(I[x[0]] / blk, I[x[1]] ^ -(I[x[0]] / blk & 1))
-	iota(ALL(s), 0);
-	sort(ALL(s), [&](ll s, ll t) { return K(Q[s]) < K(Q[t]); });
-	for (ll qi : s) fore(end,0,2) {
-		ll &a = pos[end], b = Q[qi][end], i = 0;
-#define step(c) { if (in[c]) { del(a, end); in[a] = 0; } \
-                  else { add(c, end); in[c] = 1; } a = c; }
-		while (!(L[b] <= L[a] && R[a] <= R[b]))
-			I[i++] = b, b = par[b];
-		while (a != b) step(par[a]);
-		while (i--) step(I[i]);
-		if (end) res[qi] = calc();
+	HashInterval hi(a);
+	SQRT::MoQueries sq{a, pws};
+	Hilbert::MoQueries hb{a, pws};
+
+	vector<H> true_sol;
+	for(auto& [l, r] : qs) {
+		true_sol.pb(hi.hashInterval(l, r));
 	}
-	return res;
+	vector<H> sq_sol = sq.solve(SZ(a), qs), hb_sol = hb.solve(SZ(a), qs);
+
+	assert(true_sol == sq_sol);
+	assert(true_sol == hb_sol);
 }
 
-}
-
-void testTr(ll n, ll q) {
-	ops = 0;
-	blk = max((ll)(n / sqrt(max(q, 1ll))), 1ll);
-	vector<array<ll, 2>> queries(q);
-	for (auto& pa : queries) {
-		pa[0] = rand() % n;
-		pa[1] = rand() % n;
-	}
-	vi par(n), val(n);
-	fore(i,1,n) par[i] = rand() % i;
-	fore(i,0,n) val[i] = rand() % 1000;
-	vector<vi> ed(n);
-	fore(i,1,n) ed[par[i]].pb(i), ed[i].pb(par[i]);
-	MoTree::vals = val;
-	MoTree::sum = 0;
-	MoTree::path.clear();
-	vi res = MoTree::moTree(queries, ed);
-	vi seen(n);
-	fore(i,0,q) {
-		// Tree depth is logarithmic, so compute query answers naively
-		ll l = queries[i][0], r = queries[i][1];
-		ll at = l;
-		while (at != 0) seen[at] = 1, at = par[at];
-		seen[at] = 1;
-		ll sum = 0;
-		while (!seen[r]) sum += val[r], r = par[r];
-		at = l;
-		while (at != 0) seen[at] = 0, at = par[at];
-		seen[at] = 0;
-		while (l != r) sum += val[l], l = par[l];
-		sum += val[l];
-		assert(res[i] == sum);
-	}
-}
+random_device rd;
+mt19937_64 gen(rd());
 
 int main() {
-	srand(2);
-	fore(it,0,10) fore(n,1,15) fore(q,0,n*n) {
-		testTr(n, q);
+	ll maxn = 1e4, maxq = 1e5;
+	vector<H> pws(maxn);
+	pws[0] = 1;
+	fore(i, 1, maxn) pws[i] = pws[i-1] * C;
+
+	// Edge cases
+	fore(_, 0, 20) fore(n, 0, 10) fore(q, 0, n*n)
+		test(n, q, pws);
+
+	// Random cases
+	fore(_, 0, 200) {
+		ll n = randin(1ll, maxn), q = randin(1ll, maxq);
+		test(n, q, pws);
 	}
-	testTr(100'000, 100'000);
-	testTr(1000, 100'000);
-	testTr(100'000, 1000);
-	test(100'000, 100'000);
-	test(1000, 100'000);
-	test(100'000, 1000);
-	fore(it,0,10) fore(n,1,15) fore(q,0,n*n) {
-		test(n, q);
-	}
-	cout << "Tests passed!" << endl;
+
+	cout << "Tests passed" << endl;
 }
