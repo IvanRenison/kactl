15493582e1ffc188656e176842a9dab9368c7d9e
Pietro Palombini <52302877+Aplietexe@users.noreply.github.com>
2024-11-05 09:16:24 -0300

    Add new MoQueries and format MoTree (#94)
    Co-authored-by: Iván Renison <85908989+IvanRenison@users.noreply.github.com>

--- content/data-structures/MoQueries.h
+++ content/data-structures/MoQueries.h
@@ -1,62 +1,55 @@
 /**
- * Author: Simon Lindholm
- * Date: 2019-12-28
+ * Author: Pietro Palombini
+ * Date: 2024-11-03
  * License: CC0
- * Source: https://github.com/hoke-t/tamu-kactl/blob/master/content/data-structures/MoQueries.h
- * Description: Answer interval or tree path queries by finding an approximate TSP through the queries,
- * and moving from one query to the next by adding/removing points at the ends.
- * If values are on tree edges, change \texttt{step} to add/remove the edge $(a, c)$ and remove the initial \texttt{add} call (but keep \texttt{in}).
- * Time: O(N \sqrt Q)
- * Status: stress-tested
+ * Source: https://codeforces.com/blog/entry/61203/
+ * Description: Answer range queries offline using Hilbert
+ * order or SQRT decomposition. For Hilbert, change lines with
+ * \texttt{SQ} for commented lines. Define \texttt{add},
+ * \texttt{rem}, \texttt{calc}, add necessary fields to
+ * \texttt{MoQueries}, instantiate, and call \texttt{solve}.
+ * Queries are given as pairs $[l, r)$. \texttt{end} is true if
+ * the element should be added/removed from the end of the
+ * range. \texttt{qid} is the query index. Starts at [0, 0).
+ * Time: Fast O(T N \sqrt Q + CQ) where T is the cost of
+ * \texttt{add} and \texttt{rem} and C is the cost of
+ * \texttt{calc}.
  */
+
 #pragma once
 
-void add(ll ind, ll end) {} // add a[ind] (end = 0 or 1)
-void del(ll ind, ll end) {} // remove a[ind]
-ll calc() { return 0; } // compute current answer
-
-vi mo(vector<ii> Q) {
-	ll L = 0, R = 0, blk = 350; // ~N/sqrt(Q)
-	vi s(SZ(Q)), res = s;
-#define K(x) ii(x.fst/blk, x.snd ^ -(x.fst/blk & 1))
-	iota(ALL(s), 0);
-	sort(ALL(s), [&](ll s, ll t) { return K(Q[s]) < K(Q[t]); });
-	for (ll qi : s) {
-		ii q = Q[qi];
-		while (L > q.fst) add(--L, 0);
-		while (R < q.snd) add(R++, 1);
-		while (L < q.fst) del(L++, 0);
-		while (R > q.snd) del(--R, 1);
-		res[qi] = calc();
+constexpr ll B = 447; // ~N/sqrt(Q)
+struct MoQueries {
+	typedef ll T;
+	void add(ll pos, bool end, ll qid) {}
+	void rem(ll pos, bool end, ll qid) {}
+	T calc() { return 0; }
+	ii k(ii &x) { return {x.fst/B, x.snd ^ -(x.fst/B&1)};} // SQ
+	vector<T> solve(ll n, vector<ii> &qs) {
+		ll l = 0, r = 0, q = SZ(qs); //, rx, ry, k, s;
+		vi p(q); //, o(q);
+		iota(ALL(p), 0);
+	//fore(i, 0, q) {
+	//	auto [x, y] = qs[i];
+	//	for (k = s = bit_ceil((unsigned)n); s >>= 1;) {
+	//		rx = (x&s)>0, ry = (y&s)>0, o[i] += s*s*((rx*3)^ry);
+	//		if (!ry) {
+	//			if (rx) x = k - 1 - x, y = k - 1 - y;
+	//			swap(x, y);
+	//		}
+	//	}
+	//}
+	//sort(ALL(p), [&](ll i, ll j) { return o[i] < o[j]; });
+		sort(ALL(p),[&](ll i,ll j){return k(qs[i])<k(qs[j]);});//SQ
+		vector<T> res(q);
+		for (ll i : p) {
+			auto [ql, qr] = qs[i];
+			while (l > ql) add(--l, 0, i);
+			while (r < qr) add(r++, 1, i);
+			while (l < ql) rem(l++, 0, i);
+			while (r > qr) rem(--r, 1, i);
+			res[i] = calc();
+		}
+		return res;
 	}
-	return res;
-}
-
-vi moTree(vector<array<ll, 2>> Q, vector<vi>& ed, ll root=0) {
-	ll N = SZ(ed), pos[2] = {}, blk = 350; // ~N/sqrt(Q)
-	vi s(SZ(Q)), res = s, I(N), L(N), R(N), in(N), par(N);
-	add(0, 0), in[0] = 1;
-	auto dfs = [&](ll x, ll p, ll dep, auto&& f) -> void {
-		par[x] = p;
-		L[x] = N;
-		if (dep) I[x] = N++;
-		for (ll y : ed[x]) if (y != p) f(y, x, !dep, f);
-		if (!dep) I[x] = N++;
-		R[x] = N;
-	};
-	dfs(root, -1, 0, dfs);
-#define K(x) ii(I[x[0]] / blk, I[x[1]] ^ -(I[x[0]] / blk & 1))
-	iota(ALL(s), 0);
-	sort(ALL(s), [&](ll s, ll t) { return K(Q[s]) < K(Q[t]); });
-	for (ll qi : s) fore(end,0,2) {
-		ll &a = pos[end], b = Q[qi][end], i = 0;
-#define step(c) { if (in[c]) { del(a, end); in[a] = 0; } \
-                  else { add(c, end); in[c] = 1; } a = c; }
-		while (!(L[b] <= L[a] && R[a] <= R[b]))
-			I[i++] = b, b = par[b];
-		while (a != b) step(par[a]);
-		while (i--) step(I[i]);
-		if (end) res[qi] = calc();
-	}
-	return res;
-}
+};
---
+++ stress-tests/data-structures/MoTree.cpp
@@ -0,0 +1,100 @@
+#include "../utilities/template.h"
+
+#include "../../content/various/Randin.h"
+
+/// content/data-structures/MoTree.h
+/// START diff
+ll B = 447; // ~N/sqrt(Q)
+/// END diff
+struct MoTree {
+	typedef ll T;
+	/// START diff
+	vi &vals;
+	ll sum = 0;
+	deque<ll> path;
+	void add(ll pos, ll end) {
+		sum += vals[pos];
+		if (end == 0) path.push_front(pos);
+		else path.pb(pos);
+	}
+	void del(ll pos, ll end) {
+		sum -= vals[pos];
+		assert(!path.empty());
+		if (end == 0) {
+			assert(path.front() == pos);
+			path.pop_front();
+		} else {
+			assert(path.back() == pos);
+			path.pop_back();
+		}
+	}
+	T calc() { return sum; }
+	/// END diff
+	vector<T> solve(vector<ii> Q, vector<vi>& g, ll root=0) {
+		ll N = SZ(g), pos[2] = {};
+		vi s(SZ(Q)), I(N), L(N), R(N), in(N), par(N);
+		vector<T> res(SZ(Q));
+		add(0, 0), in[0] = 1;
+		auto dfs = [&](ll x, ll p, ll dep, auto&& f) -> void {
+			par[x] = p, L[x] = dep ? I[x] = N++ : N;
+			for (ll y : g[x]) if (y != p) f(y, x, !dep, f);
+			R[x] = !dep ? I[x] = ++N : N;
+		};
+		dfs(root, -1, 0, dfs);
+		#define K(x) ii(I[x.fst]/B, I[x.snd] ^ -(I[x.fst]/B & 1))
+		iota(ALL(s), 0);
+		sort(ALL(s), [&](ll s, ll t) {return K(Q[s]) < K(Q[t]);});
+		for (ll qi : s) fore(e,0,2) {
+			ll &a = pos[e], b = e ? Q[qi].fst : Q[qi].snd, i = 0;
+#define step(c)in[c]?(del(a,e),in[a]=0):(add(c,e),in[c]=1),a=c;
+			while (L[a] < L[b] || R[b] < R[a]) b = par[I[i++] = b];
+			while (a != b) step(par[a]);
+			while (i--) step(I[i]);
+			if (e) res[qi] = calc();
+		}
+		return res;
+	}
+};
+/// END content
+
+void testTr(ll n, ll q) {
+	B = max((ll)(n / sqrt(max(q, 1ll))), 1ll);
+	vector<ii> queries(q);
+	for (auto &[l, r] : queries) {
+		l = randin(0ll, n);
+		r = randin(0ll, n);
+	}
+	vi par(n), val(n);
+	fore(i,1,n) par[i] = randin(0ll, i);
+	fore(i,0,n) val[i] = randin(0ll, 1000ll);
+	vector<vi> g(n);
+	fore(i,1,n) g[par[i]].pb(i), g[i].pb(par[i]);
+	MoTree moTree({val});
+	vi res = moTree.solve(queries, g);
+	vi seen(n);
+	fore(i,0,q) {
+		// Tree depth is logarithmic, so compute query answers naively
+		ll l = queries[i].fst, r = queries[i].snd;
+		ll at = l;
+		while (at != 0) seen[at] = 1, at = par[at];
+		seen[at] = 1;
+		ll sum = 0;
+		while (!seen[r]) sum += val[r], r = par[r];
+		at = l;
+		while (at != 0) seen[at] = 0, at = par[at];
+		seen[at] = 0;
+		while (l != r) sum += val[l], l = par[l];
+		sum += val[l];
+		assert(res[i] == sum);
+	}
+}
+
+int main() {
+	fore(it,0,10) fore(n,1,15) fore(q,0,n*n) {
+		testTr(n, q);
+	}
+	testTr(100'000, 100'000);
+	testTr(1000, 100'000);
+	testTr(100'000, 1000);
+	cout << "Tests passed!" << endl;
+}
