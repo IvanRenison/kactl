25e9b747be9c59fa8c11ad8052adbd9d540b6a42
Pietro Palombini <pietropalo@icloud.com>
2024-11-05 03:40:47 -0300

    Use tabs

--- content/number-theory/PointsUnderLine.h
+++ content/number-theory/PointsUnderLine.h
@@ -14,14 +14,14 @@
 #pragma once
 
 ll f(ll a, ll b, ll c) {
-  if (c <= 0) return 0;
-  if (a < b) swap(a, b);
-  ll m = c / a;
-  if (a == b) return m * (m - 1) / 2;
-  ll k = (a - 1) / b, h = (c - a * m) / b;
-  return f(b, a - b*k, c - b*(k*m + h)) + k*m*(m - 1)/2 + m*h;
+	if (c <= 0) return 0;
+	if (a < b) swap(a, b);
+	ll m = c / a;
+	if (a == b) return m * (m - 1) / 2;
+	ll k = (a - 1) / b, h = (c - a * m) / b;
+	return f(b, a - b*k, c - b*(k*m + h)) + k*m*(m - 1)/2 + m*h;
 }
 ll g(ll a, ll b, ll c, ll X, ll Y) {
-  if (a * X + b * Y <= c) return X * Y;
-  return f(a,b,c)-f(a,b,c-a*X)-f(a,b,c-b*Y)+f(a,b,c-a*X-b*Y);
+	if (a * X + b * Y <= c) return X * Y;
+	return f(a,b,c)-f(a,b,c-a*X)-f(a,b,c-b*Y)+f(a,b,c-a*X-b*Y);
 }
