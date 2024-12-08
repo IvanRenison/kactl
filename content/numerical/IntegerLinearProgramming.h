dcfa9d410a5f869e2d2cda9d55041464533eac85
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-12-08 12:05:42 -0300

    Update

--- content/numerical/IntegerLinearProgramming.h
+++ content/numerical/IntegerLinearProgramming.h
@@ -5,6 +5,5 @@
  * Description: When $A$ and $b$ have all integer entries and $A$ is totally unimodular then
  * every basic feasible solution is integral so the simplex algorithm can be used to solve ILP.
  * A matrix $A$ is totally unimodular if every square submatrix of $A$ has determinant $0$, $1$ or $-1$.
- * Details: Is there some time guarantee for this?
  * Status: untested
  */
